92a9e7dd6e60fe43c003b3ec9ac9eb0a1a6e0d55
石毛秀悟 <ishigeshuugo@shuugoea.local>
2024-08-07 23:57:57 +0900

    Create regular_linux_programming.h

---
+++ regular_linux_programming.h
@@ -0,0 +1,16 @@
+#ifndef REGULAR_LINUX_PROGRAMMING_H
+# define REGULAR_LINUX_PROGRAMMING_H
+
+# include <unistd.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <stdbool.h>
+
+# define BUFFER_SIZE 2048
+
+#endif
+
